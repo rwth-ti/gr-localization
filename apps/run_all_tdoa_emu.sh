4a279f419b7cc1ed2b7ad117d5b36f8f77dec303
Felix Bartsch <bartsch@ti.rwth-aachen.de>
2016-12-02 13:21:58 +0100

    Change default positions in emu script, Bugfixes
    Bugfix in fusion_center

--- apps/run_all_tdoa_emu.sh
+++ apps/run_all_tdoa_emu.sh
@@ -2,8 +2,8 @@
 
 killall python
 gnome-terminal --tab -e "bash -c \"./fusion_center.py; exec bash\"" --title "Fusion center" \
---tab -e "bash -c \"./receiver_emu.py -i 1 --coordinates-m 45,30 --movement-file ../movements/spline_paper.csv; exec bash\"" --title "Receiver 1" \
---tab -e "bash -c \"./receiver_emu.py -i 2 --coordinates-m 250,30 --movement-file ../movements/spline_paper.csv; exec bash\"" --title "Receiver 2" \
+--tab -e "bash -c \"./receiver_emu.py -i 1 --coordinates-m 30,30 --movement-file ../movements/spline_paper.csv; exec bash\"" --title "Receiver 1" \
+--tab -e "bash -c \"./receiver_emu.py -i 2 --coordinates-m 200,30 --movement-file ../movements/spline_paper.csv; exec bash\"" --title "Receiver 2" \
 --tab -e "bash -c \"./receiver_emu.py -i 3 --coordinates-m 120,150 --movement-file ../movements/spline_paper.csv; exec bash\"" --title "Receiver 3"
 pwd
 ./gui.py
