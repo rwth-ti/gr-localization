f12e814313931bdef8d924606f616f11d462854e
Felix Bartsch <bartsch@ti.rwth-aachen.de>
2017-05-19 13:51:43 +0200

    Change run scripts

--- apps/run_all_tdoa_emu_4rx.sh
+++ apps/run_all_tdoa_emu_4rx.sh
@@ -2,9 +2,10 @@
 
 killall python
 gnome-terminal --tab -e "bash -c \"./fusion_center.py; exec bash\"" --title "Fusion center" \
---tab -e "bash -c \"./receiver_emu.py -i 1 --coordinates-m 30,80 --movement-file ../movements/spline_paper.csv; exec bash\"" --title "Receiver 1" \
---tab -e "bash -c \"./receiver_emu.py -i 2 --coordinates-m 200,80 --movement-file ../movements/spline_paper.csv; exec bash\"" --title "Receiver 2" \
---tab -e "bash -c \"./receiver_emu.py -i 3 --coordinates-m 120,150 --movement-file ../movements/spline_paper.csv; exec bash\"" --title "Receiver 3" \
---tab -e "bash -c \"./receiver_emu.py -i 4 --coordinates-m 120,20 --movement-file ../movements/spline_paper.csv; exec bash\"" --title "Receiver 4"
+--tab -e "bash -c \"./receiver_emu.py -i 1 --coordinates-m 30,80 --coordinates-wgs84 50.77890891753905,6.061026275464247 --movement-file ../movements/spline_paper.csv; exec bash\"" --title "Receiver 1" \
+--tab -e "bash -c \"./receiver_emu.py -i 2 --coordinates-m 200,80 --coordinates-wgs84 50.77890763166232,6.063436569767161 --movement-file ../movements/spline_paper.csv; exec bash\"" --title "Receiver 2" \
+--tab -e "bash -c \"./receiver_emu.py -i 3 --coordinates-m 120,150 --coordinates-wgs84 50.77953748977171,6.062303149728518 --movement-file ../movements/spline_paper.csv; exec bash\"" --title "Receiver 3" \
+--tab -e "bash -c \"./receiver_emu.py -i 4 --coordinates-m 120,20 --coordinates-wgs84 50.77836888854142,6.06230159702876 --movement-file ../movements/spline_paper.csv; exec bash\"" --title "Receiver 4"
 pwd
 ./gui.py
+
--- apps/run_all_tdoa_emu_selfloc.sh
+++ apps/run_all_tdoa_emu_selfloc.sh
@@ -2,12 +2,12 @@
 
 killall python
 gnome-terminal --tab -e "bash -c \"./fusion_center.py; exec bash\"" --title "Fusion center" 
-gnome-terminal --tab -e "bash -c \"./receiver_emu.py -i 1 --coordinates-m 30,80 --movement-file ../movements/selfloc.csv; exec bash\"" --title "Receiver 1" 
+gnome-terminal --tab -e "bash -c \"./receiver_emu.py -i 1 --coordinates-m 118.59,105.15 --coordinates-wgs84 50.77913433,6.06228259 --movement-file ../movements/selfloc_average.csv; exec bash\"" --title "Receiver 1" 
 sleep 1s 
-gnome-terminal --tab -e "bash -c \"./receiver_emu.py -i 2 --coordinates-m 200,80 --movement-file ../movements/selfloc.csv; exec bash\"" --title "Receiver 2"
+gnome-terminal --tab -e "bash -c \"./receiver_emu.py -i 2 --coordinates-m 115.07,71.62 --coordinates-wgs84 50.77883295,6.06223227 --movement-file ../movements/selfloc_average.csv; exec bash\"" --title "Receiver 2"
 sleep 1s 
-gnome-terminal --tab -e "bash -c \"./receiver_emu.py -i 3 --coordinates-m 120,150 --movement-file ../movements/selfloc.csv; exec bash\"" --title "Receiver 3"
+gnome-terminal --tab -e "bash -c \"./receiver_emu.py -i 3 --coordinates-m 136.57,65.76 --coordinates-wgs84 50.77878010,6.06253711 --movement-file ../movements/selfloc_average.csv; exec bash\"" --title "Receiver 3"
 sleep 1s 
-gnome-terminal --tab -e "bash -c \"./receiver_emu.py -i 4 --coordinates-m 120,20 --movement-file ../movements/selfloc.csv; exec bash\"" --title "Receiver 4"
+gnome-terminal --tab -e "bash -c \"./receiver_emu.py -i 4 --coordinates-m 151.45,93.28 --coordinates-wgs84 50.77902740,6.06274836 --movement-file ../movements/selfloc_average.csv; exec bash\"" --title "Receiver 4"
 pwd
 ./gui.py
