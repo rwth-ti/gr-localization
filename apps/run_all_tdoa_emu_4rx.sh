b5fee6a51c04b26fc583e95e26bd278b1dd1e63c
Felix Bartsch <bartsch@ti.rwth-aachen.de>
2017-05-31 14:03:33 +0200

    Adjust defaults in emulator run scripts

--- apps/run_all_tdoa_emu_4rx.sh
+++ apps/run_all_tdoa_emu_4rx.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 killall python
-gnome-terminal --tab -e "bash -c \"./fusion_center.py; exec bash\"" --title "Fusion center" \
+gnome-terminal --tab -e "bash -c \"./fusion_center.py --num-samples 300; exec bash\"" --title "Fusion center" \
 --tab -e "bash -c \"./receiver_emu.py -i 1 --coordinates-m 30,80 --coordinates-wgs84 50.77890891753905,6.061026275464247 --movement-file ../movements/spline_paper.csv; exec bash\"" --title "Receiver 1" \
 --tab -e "bash -c \"./receiver_emu.py -i 2 --coordinates-m 200,80 --coordinates-wgs84 50.77890763166232,6.063436569767161 --movement-file ../movements/spline_paper.csv; exec bash\"" --title "Receiver 2" \
 --tab -e "bash -c \"./receiver_emu.py -i 3 --coordinates-m 120,150 --coordinates-wgs84 50.77953748977171,6.062303149728518 --movement-file ../movements/spline_paper.csv; exec bash\"" --title "Receiver 3" \
